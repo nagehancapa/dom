7c7206d5a203fa2712324ad106b4a6d1da62a5f9
nagehancapa <capanagehan@gmail.com>
2021-09-30 11:11:55 +0200

    added querySelectorAll example

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 function main() {
-  const student = document.querySelector(".subtitle");
-  console.log(student);
+  const paragraphs = document.querySelectorAll("p");
+  paragraphs.forEach((paragraph) => console.log(paragraph));
 }
 
-// check if elements are still loading
 if (document.readyState === "loading") {
-  // wait for the DOMContentLoaded event
-  console.log("waiting");
   document.addEventListener("DOMContentLoaded", main);
 } else {
-  console.log("loaded");
   main();
 }
